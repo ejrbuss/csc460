0b858d0599a244cd24e868c2d7f146e0ad60aad2
torrey <trandolph123@gmail.com>
2019-01-29 17:24:40 -0800

    trying to get joystick to toggle LED/Laser

--- project-1/src/project-1.cpp
+++ project-1/src/project-1.cpp
@@ -2,14 +2,49 @@
 #include <SoftwareSerial.h>
 #include <HardwareSerial.h>
 
-void setup() {
-    Serial.begin(9600);
-    Serial.println("Hello, World!");
+#define LED 13
+#define MIN 0
+#define MAX 255
+#define JOY_SW 2
+#define BAUD 9600
+
+volatile bool on = false;
+volatile bool isr_triggered = false;
+
+void ISR_TOGGLE_LASER() {
+    noInterrupts();
+    isr_triggered = true;
+    Serial.println("IN ISR!!");
+    if (on) {
+        digitalWrite(LED, LOW);
+        on = false;
+    } else {
+        digitalWrite(LED, HIGH);
+        on = true;
+    }
+    interrupts();
 }
 
-void loop() { // run over and over
-    if (Serial.available()) {
-        Serial.print(Serial.read());
-        delay(5);
+void setup() {
+    pinMode(JOY_SW, INPUT_PULLUP);
+    Serial.begin(BAUD);
+
+    attachInterrupt(digitalPinToInterrupt(JOY_SW), ISR_TOGGLE_LASER, FALLING);
+
+    for (;;) {
+        loop();
     }
+}
+
+void loop() {
+    if (isr_triggered) {
+        noInterrupts();
+        isr_triggered = false;
+        Serial.println("delaying..");
+        delay(1000);
+        Serial.println("enabling interrupts again");
+        Serial.println();
+        interrupts();
+    }
+
 }
