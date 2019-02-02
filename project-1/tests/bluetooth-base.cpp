5f7581edc740ba4f0e237c05abd5aa016dd20249
ejrbuss <ejrbuss@gmail.com>
2019-02-02 13:22:37 -0800

    bluetooth test now works

--- project-1/tests/bluetooth-base.cpp
+++ project-1/tests/bluetooth-base.cpp
@@ -2,9 +2,9 @@
 #include "Peripherals.h"
 
 int main() {
+    init_arduino();
     Serial1.begin(SERIAL_BAUD);
     LiquidCrystal lcd = init_lcd();
-
     // Send the messagee
     const char * msg = "Hello!";
     Serial1.write(msg);
@@ -15,7 +15,7 @@ int main() {
 
     for (;;) {
         if (Serial1.available()) {
-          lcd.write(Serial1.read());
+            lcd.write(Serial1.read());
         }
     }
 
