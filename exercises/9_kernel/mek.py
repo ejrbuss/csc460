0d94774b29768715e8ce253b34793a8a52537290
ejrbuss <ejrbuss@gmail.com>
2019-02-25 17:03:48 -0800

    exercisee 9 initial code

---
+++ exercises/9_kernel/mek.py
@@ -0,0 +1,33 @@
+from os.path import join
+
+# This is a standard configuration file for mekpie
+
+# the name of your project
+name = '9_kernel'
+# the c compiler configuration to use (gcc_clang, avr_gcc, or emscripten)
+cc = avr_gcc(hardware='atmega2560', programmer='wiring', baud='115200')
+# any libraries to load
+libs = []
+# additional compiler flags
+flags = [
+    # definitions
+    '-DF_CPU=16000000L',
+    '-DARDUINO=10808',
+    '-DARDUINO_AVR_MEGA2560',
+    '-DARDUINO_ARCH_AVR',
+    # Need optimizations for delay to work
+    '-Os',
+    # Reduce application size and optimize
+    '-flto',
+    '-ffunction-sections',
+    '-fdata-sections',
+    '-fno-fat-lto-objects',
+    '-fuse-linker-plugin',
+]
+
+main = 'active.c'
+
+if options.release:
+    flags += ['-Wall', '-O']
+else:
+    flags += ['-Wall', '-g']
